3d6138c61f48ae14263d1665aa811b318ebf3500
Tomek <tomek.ogorek@netblink.net>
2019-11-22 13:21:25 +0100

    (decorators) fix

--- optimization_helper/decorators.py
+++ optimization_helper/decorators.py
@@ -10,7 +10,7 @@ class optimize_related(object):
 
     def __call__(self, target):
         target._optimize_related = dict(
-            select_related=self.select_related,
-            prefetch_related=self.prefetch_related
+            select_related=self.select_related or "",
+            prefetch_related=self.prefetch_related or ""
         )
         return target
